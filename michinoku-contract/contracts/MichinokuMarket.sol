fc789d06a26955b72aac4b99806bd77fe70f0043
kkksb <kkex5027@gmail.com>
2022-06-01 21:21:49 +0900

    add: Mint From Metadata

--- michinoku-contract/contracts/MichinokuMarket.sol
+++ michinoku-contract/contracts/MichinokuMarket.sol
@@ -1,13 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
 
-contract MichinokuMarket {
-  string defaultMessage; 
+pragma solidity ^0.8.0;
 
-  constructor() public {
-    defaultMessage = 'Welcome to MichinokuDAO';
-  }
-  function getMessage() public view returns(string memory) {
-    return defaultMessage;
-  }
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MichinokuMarket is ERC721URIStorage {
+    mapping(uint256 => string) public tokenName;
+    uint256 public tokenCounter;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        tokenCounter = 0;
+    }
+
+    function createToken(uint256 tokenId, string memory name) external {
+        _safeMint(msg.sender, tokenId);
+        tokenName[tokenId] = name;
+    }
+
+    function getTokenName(uint256 tokenId)
+        external
+        view
+        returns (string memory)
+    {
+        return tokenName[tokenId];
+    }
+
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newItemId;
+    }
 }
